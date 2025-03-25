534495da113f06c4a97486fc74bde2ee2ff9b26b
UserDeJs <trabalhosgithubgf@gmail.com>
2025-03-25 14:11:19 +0000

    Davi ama homens

---
+++ variaveis.js
@@ -0,0 +1,23 @@
+var nome = "Gustavo"
+let idade = 17
+const signo = "virgem"
+
+/*
+A diferença entre estas formas de variáveis é complexidade entre a possibiidade de escrever sobre variáveis anteriores e criar novas.
+
+Var
+É a forma mais antiga de declarar variáveis no JavaScript 
+Tem escopo global ou de função, ou seja, pode ser acessada de qualquer lugar dentro da função onde foi declarada 
+Pode ser atualizada e declarada novamente dentro de seu escopo 
+
+Let
+Tem escopo de bloco, ou seja, só pode ser acessada dentro do bloco onde foi declarada 
+Pode ser atualizada, mas não pode ser declarada novamente 
+É preferida em códigos grandes, criados por vários programadores, para evitar que duas variáveis distintas sejam declaradas com o mesmo nome 
+
+Const 
+Não pode ser atualizada nem declarada novamente
+Deve ser inicializada no momento da declaração
+É utilizada para valores constantes, como o valor de pi
+
+*/
